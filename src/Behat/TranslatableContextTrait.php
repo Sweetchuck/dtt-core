2ab8d9a4c7d00101ff0ce5b4fa7368647af96ce8
Andor <david-andor@kozpontiagy.hu>
2023-10-26 20:52:38 +0200

    Reemove sweetchuck/utils

--- src/Behat/TranslatableContextTrait.php
+++ src/Behat/TranslatableContextTrait.php
@@ -47,12 +47,71 @@ trait TranslatableContextTrait {
       $reflection = new \ReflectionClass(static::class);
 
       static::$i18nDirs[$prefix] = Path::join(
-        FilesystemUtils::findFileUpward($composerFileName, Path::getDirectory($reflection->getFileName())),
+        static::findFileUpward($composerFileName, Path::getDirectory($reflection->getFileName())),
         'i18n',
       );
     }
 
     return Path::join(static::$i18nDirs[$prefix], (string) end($namespaceParts));
+  }
+
+  /**
+   * @param string $fileName
+   * @param string $currentDir
+   * @param null|string $rootDir
+   *   Do not go above this directory.
+   *
+   * @return null|string
+   *   Returns NULL if the $fileName not exists in any of the parent directories,
+   *   returns the parent directory without the $fileName if the $fileName
+   *   exists in one of the parent directory.
+   *
+   * @link https://github.com/Sweetchuck/utils/blob/1.x/src/Filesystem.php
+   */
+  protected static function findFileUpward(
+    string $fileName,
+    string $currentDir,
+    ?string $rootDir = null
+  ): ?string {
+    if ($rootDir !== null && !static::isParentDirOrSame($rootDir, $currentDir)) {
+      throw new \InvalidArgumentException("The '$rootDir' is not parent dir of '$currentDir'");
+    }
+
+    while ($currentDir && ($rootDir === null || static::isParentDirOrSame($rootDir, $currentDir))) {
+      if (file_exists("$currentDir/$fileName")) {
+        return $currentDir;
+      }
+
+      $parentDir = Path::getDirectory($currentDir);
+      if ($currentDir === $parentDir) {
+        break;
+      }
+
+      $currentDir = $parentDir;
+    }
+
+    return null;
+  }
+
+  /**
+   * @link https://github.com/Sweetchuck/utils/blob/1.x/src/Filesystem.php
+   */
+  protected static function isParentDirOrSame(string $parentDir, string $childDir): bool
+  {
+    # @todo Handle a/./b and a/../c formats.
+    if ($parentDir === '.') {
+      $parentDir = './';
+    }
+
+    if ($childDir === '.') {
+      $childDir = './';
+    }
+
+    $parentDir = preg_replace('@^\./@', '', $parentDir);
+    $childDir = preg_replace('@^\./@', '', $childDir);
+    $pattern = '@^' . preg_quote($parentDir, '@') . '(/|$)@';
+
+    return (bool) preg_match($pattern, $childDir);
   }
 
 }
