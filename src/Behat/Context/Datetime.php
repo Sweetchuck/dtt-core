750e8f6a58dee2bc4f38d141e4c387216e3e09f6
Andor <david-andor@kozpontiagy.hu>
2023-12-04 12:49:09 +0100

    Support for "date only" in datetime fields

--- src/Behat/Context/Datetime.php
+++ src/Behat/Context/Datetime.php
@@ -7,6 +7,7 @@ namespace Sweetchuck\DrupalTestTraits\Core\Behat\Context;
 use Behat\Gherkin\Node\TableNode;
 use Behat\Mink\Element\ElementInterface;
 use Behat\Mink\Element\NodeElement;
+use Behat\Mink\Exception\ElementNotFoundException;
 use Behat\Mink\Exception\ExpectationException;
 use Sweetchuck\DrupalTestTraits\Core\Utils;
 
@@ -37,6 +38,21 @@ class Datetime extends Base {
 
   /**
    * @When I fill :field date range field with:
+   *
+   * @code
+   * When I fill "My datetime label" date range field with:
+   *   | from                | to                  |
+   *   | 2023-11-26T01:02:03 | 2023-11-27T04:05:06 |
+   * @endcode
+   *
+   * @code
+   * When I fill "My date label" date range field with:
+   *   | from       | to         |
+   *   | 2023-11-26 | 2023-11-27 |
+   * @endcode
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   * @throws \Behat\Mink\Exception\ExpectationException
    */
   public function doDateRangeFieldWidgetDefaultFill(string $field, TableNode $table) {
     $baseDatetime = new \DateTime();
@@ -44,7 +60,10 @@ class Datetime extends Base {
     $wrapper = $this->findDateRangeFieldWidgetDefaultWrapperByFieldLabel($field, $wrappers);
     if (!$wrapper) {
       // @todo Find it by field machine-name.
-      throw new ExpectationException('Date range fieldWidget.default element not found', $this->getSession());
+      throw new ExpectationException(
+        'Date range fieldWidget.default element not found',
+        $this->getSession(),
+      );
     }
 
     $values = $this->convertDateRangeTableNodeToFieldValues($baseDatetime, $table);
@@ -99,25 +118,81 @@ class Datetime extends Base {
     return NULL;
   }
 
+  /**
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   */
   public function dateRangeFieldWidgetDefaultFillValues(array $values, NodeElement $wrapper) {
     foreach ($values as $delta => $value) {
-      $wrapper
-        ->find('css', "*[name\$=\"[$delta][value][date]\"]")
-        ->setValue(substr((string) $value['value'], 0, 10));
-      $wrapper
-        ->find('css', "*[name\$=\"[$delta][value][time]\"]")
-        ->setValue(substr((string) $value['value'], 11));
+      $this->dateRangeFieldWidgetDefaultFillValuePart(
+        $wrapper,
+        $delta,
+        'value',
+        $value,
+      );
 
-      $wrapper
-        ->find('css', "*[name\$=\"[$delta][end_value][date]\"]")
-        ->setValue(substr((string) $value['end_value'], 0, 10));
-      $wrapper
-        ->find('css', "*[name\$=\"[$delta][end_value][time]\"]")
-        ->setValue(substr((string) $value['end_value'], 11));
+      $this->dateRangeFieldWidgetDefaultFillValuePart(
+        $wrapper,
+        $delta,
+        'end_value',
+        $value,
+      );
 
       // @todo Support multi-value field.
       break;
     }
+
+    return $this;
+  }
+
+  /**
+   * @param \Behat\Mink\Element\NodeElement $wrapper
+   * @param string $part
+   *   Allowed values: value|end_value.
+   * @param array $value
+   *   - value.
+   *   - end_value.
+   *
+   * @return $this
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   */
+  public function dateRangeFieldWidgetDefaultFillValuePart(
+    NodeElement $wrapper,
+    int $delta,
+    string $part,
+    array $value,
+  ) {
+    $selector = 'css';
+
+    $locator = "*[name\$=\"[$delta][$part][date]\"]";
+    $date_element = $wrapper->find($selector, $locator);
+    $date = substr((string) $value[$part], 0, 10);
+    if (!$date_element) {
+      throw new ElementNotFoundException(
+        $this->getSession(),
+        NULL,
+        $selector,
+        $locator,
+      );
+    }
+    $date_element->setValue($date);
+
+    $locator = "*[name\$=\"[$delta][$part][time]\"]";
+    $time_element = $wrapper->find($selector, $locator);
+    $time = substr((string) $value[$part], 11);
+    if (!$time_element && $time === '00:00:00') {
+      return $this;
+    }
+
+    if (!$time_element) {
+      throw new ElementNotFoundException(
+        $this->getSession(),
+        NULL,
+        $selector,
+        $locator,
+      );
+    }
+    $time_element->setValue($time);
 
     return $this;
   }
